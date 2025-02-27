4b4e89855f96d813ed057984e4274b04b797dae2
Douglas Amilcar Bonilla lemus <165966449+Doug-Bonilla@users.noreply.github.com>
2025-02-26 22:30:29 -0800

    Added toggle button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 var fans = document.querySelector(".fans");
 var fish = document.querySelector(".fish");
 var pets = document.querySelector(".pets");
+var switchInput = document.querySelector("#toggle");
+var ball = document.querySelector("ball");
+var h1 = document.querySelector("h1");
+var body = document.querySelector("body");
 
 fans.addEventListener("mouseover", function () {
   fans.innerText = "123k";
@@ -10,4 +14,16 @@ fish.addEventListener("mouseover", function () {
 });
 pets.addEventListener("mouseover", function () {
   pets.innerText = "20k";
+});
+
+switchInput.addEventListener("click", function () {
+  if (body.classList.contains("light")) {
+    body.classList.remove("light");
+    ball.classList.remove("move-right");
+    h1.innerText = "Stealth Quincy";
+  } else {
+    body.classList.add("light");
+    ball.classList.add("move-right");
+    h1.innerText = "Party Quincy";
+  }
 });
